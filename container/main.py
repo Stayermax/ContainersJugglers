ab63ac5cb96c1b293144d4dfccdd0a75000316b4
Stayermax <vitalkrat@gmail.com>
2023-03-15 00:13:29 +0200

    done

--- container/main.py
+++ container/main.py
@@ -1,9 +1,36 @@
+import os
+
 from fastapi import FastAPI, File, UploadFile
 import yaml
 from yaml.loader import SafeLoader
 import requests
-
+import json
+import time
 app = FastAPI()
+global stop_juggle_flag
+stop_juggle_flag = False
+print(f"stop_juggle_flag was reseted")
+
+@app.get("/")
+async def main_page():
+    try:
+        with open('config.yaml') as f:
+            data = yaml.load(f, Loader=SafeLoader)
+            print(json.dumps(data, indent=4))
+            current_id = data['current_container_details']['current_id']
+            return f"Container {current_id} is running"
+    except Exception as e:
+        return e
+
+@app.get("/id")
+async def main_page():
+    try:
+        with open('config.yaml') as f:
+            data = yaml.load(f, Loader=SafeLoader)
+            current_id = data['current_container_details']['current_id']
+            return current_id
+    except Exception as e:
+        return e
 
 @app.get("/status")
 async def main_page():
@@ -16,27 +43,74 @@ async def main_page():
         container_info = f"Container {current_id} is running. Next container id: {next_id}"
         return container_info
 
-@app.get("/id")
-async def main_page():
-    try:
+@app.get("/start_juggle/")
+async def start_juggle():
+    global stop_juggle_flag
+    stop_juggle_flag = False
+    send_file()
+    return "Juggling starts"
+
+
+@app.get("/stop_juggle/")
+async def stop_juggle():
+    global stop_juggle_flag
+    stop_juggle_flag = True
+    return "Juggling stopped"
+
+
+def send_file():
+    if len(os.listdir('volume')) > 0:
         with open('config.yaml') as f:
             data = yaml.load(f, Loader=SafeLoader)
-            current_id = data['current_container_details']['current_id']
-            return current_id
-    except Exception as e:
-        return e
-
-@app.post("/files/")
-async def create_file(file: bytes = File()):
-    if not file:
-        return {"message": "No file sent"}
-    else:
-        return {"file_size": len(file)}
+            next_url = f"http://{data['next_container_details']['next_url']}:{data['next_container_details']['next_port']}/uploadfile/"
+        file = {'file': open('volume/ball.txt', 'rb')}
+        try:
+            resp = requests.post(url=next_url, files=file, timeout=0.5)
+        except:
+            pass
+        os.remove("volume/ball.txt")
+        return "Ball was juggled"
 
 
 @app.post("/uploadfile/")
-async def create_upload_file(file: UploadFile):
-    if not file:
-        return {"message": "No upload file sent"}
+async def get_file(file: UploadFile = File(...)):
+    global stop_juggle_flag
+    try:
+        contents = file.file.read()
+        with open(f"volume/{file.filename}", 'wb') as f:
+            f.write(contents)
+        update_counter()
+        if not stop_juggle_flag:
+            print(f"Continue juggling")
+            time.sleep(2)
+            send_file()
+        else:
+            print(f"We stopped juggling. Counter is: {get_ball_counter()}")
+    except Exception:
+        return {"message": "There was an error uploading the file"}
+    finally:
+        file.file.close()
+
+    return {"message": f"Successfully uploaded {file.filename}"}
+
+@app.get("/do_i_have_a_ball/")
+async def do_i_have_a_ball():
+    if len(os.listdir('volume')) > 0:
+        return f"Yes, I have ball, counter is {get_ball_counter()}"
     else:
-        return {"filename": file.filename}
+        return f"I don't have a ball"
+
+def get_ball_counter():
+    with open('volume/ball.txt', "r") as f:
+        counter = int(f.readline())
+    return counter
+
+def set_ball_counter(n):
+    with open('volume/ball.txt', "w") as f:
+        f.write(f"{n}")
+def update_counter():
+    n = get_ball_counter()
+    set_ball_counter(n+1)
+
+
+
